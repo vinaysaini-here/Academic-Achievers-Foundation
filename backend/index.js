f9b8644e55b4540e0d37422ff1a8b5efd6a6af84
vinaysaini-here <vinaysaini675859@gmail.com>
2025-12-13 10:27:31 +0530

    feat: Simplify CORS configuration by consolidating origin array and removing redundant code

--- backend/index.js
+++ backend/index.js
@@ -23,18 +23,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://academic-achievers-foundation1.netlify.app"
-  ],
+  origin: ["http://localhost:5173","https://academic-achievers-foundation1.netlify.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  optionsSuccessStatus: 200,
 };
-
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
-
-app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(passport.initialize());
 
